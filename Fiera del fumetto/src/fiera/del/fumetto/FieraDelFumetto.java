bca619870560dcb9d5e7d8166671b7d6559255a4
PC20 <PC20@DESKTOP-D8D9GC6.fritz.box>
2018-07-18 15:57:24 +0200

    Project FieraFumetto Alpha 0.2
    Creazione classe Fiere, Creazione ArrayList di tipo fiera con successivo popolamento.

--- Fiera del fumetto/src/fiera/del/fumetto/FieraDelFumetto.java
+++ Fiera del fumetto/src/fiera/del/fumetto/FieraDelFumetto.java
@@ -18,7 +18,18 @@ public class FieraDelFumetto implements CalendarEvent {
     public static void main(String[] args) {
         // TODO code application logic here
       
-        System.out.println("CIAO");
+        Fiera myFiera = new Fiera("Comicon", "Napoli - Mostra d'Oltremare");
+        Fiera myFiera2 = new Fiera("Anastasia-Comix", "Sant'Anastasia - Piazza Catteneo");
+        Fiera myFiera3 = new Fiera("Lucca Comics and Games", "Lucca - Centro Storico");
+        Fiera myFiera4 = new Fiera("Romics", "Roma - Fiera di Roma");
+        Fiera myFiera5 = new Fiera("FantaExpo", "Salerno");
+        ArrayList<Fiera> Fiere = new ArrayList<>();
+
+        Fiere.add(0, myFiera);
+        Fiere.add(1, myFiera2);
+        Fiere.add(2, myFiera3);
+        Fiere.add(3, myFiera4);
+        Fiere.add(4, myFiera5);;
        
 
     }          
