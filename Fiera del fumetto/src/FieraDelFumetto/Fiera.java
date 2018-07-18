76130186c0bbf496071b66d1290aa9c7d729c980
PC20 <PC20@DESKTOP-D8D9GC6.fritz.box>
2018-07-18 17:01:46 +0200

    Project Fiera Fumetto Alpha 0.3
    Agg. con Display di ArrayList e suggerimenti su denominazione delle classi da parte del committente.

--- Fiera del fumetto/src/FieraDelFumetto/Fiera.java
+++ Fiera del fumetto/src/FieraDelFumetto/Fiera.java
@@ -1,9 +1,14 @@
 package FieraDelFumetto;
 
+import calendar.api.CalendarEvent;
+import calendar.api.CalendarEventException;
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Scanner;
 
-public class Fiera {
+public class Fiera implements Comparator<Fiera>, CalendarEvent {
 
     private String nomeEvento;
     private String luogoEvento;
@@ -19,10 +24,44 @@ public class Fiera {
         this.nomeEvento = nomeEvento;
         this.luogoEvento = luogoEvento;
     }
+    
+    
+    @Override
+    public String toString(){
+       return "Fiera Fumetto{" +
+            "Nome Fiera=" + nomeEvento +
+            ", Luogo Fiera='" + luogoEvento + '\'' +
+            '}'; 
+    }
+        @Override
+        public int compare(Fiera o1, Fiera o2)
+        {
 
-    /*public  ordinaFiera(){
+            return  o1.nomeEvento.compareTo(o2.nomeEvento);
+        }
 
-    }*/
+    @Override
+    public String getTitle() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 
+    @Override
+    public LocalDate getStartDate() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 
+    @Override
+    public LocalDate getEndDate() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getCategory() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getLocation() throws CalendarEventException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
