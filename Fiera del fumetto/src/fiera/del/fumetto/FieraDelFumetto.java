4b92c09898750cf97d846a4e4d8fc71256223d0b
PC20 <PC20@DESKTOP-D8D9GC6.fritz.box>
2018-07-18 15:10:56 +0200

    Ultimo Test

--- Fiera del fumetto/src/fiera/del/fumetto/FieraDelFumetto.java
+++ Fiera del fumetto/src/fiera/del/fumetto/FieraDelFumetto.java
@@ -1,27 +1,3 @@
-<<<<<<< HEAD
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package fiera.del.fumetto;
-
-
-public class FieraDelFumetto {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-      
-        System.out.println("CIAO");
-        //test
-        //Test
-    }
-    
-}
-=======
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -42,7 +18,7 @@ public class FieraDelFumetto {
 
         //Test
         //test
+        System.out.println("Ultimo Test, tranquilli ^_^")
     }
     
 }
->>>>>>> b480b8b7787d495cb8530ee3caf358180c982465
