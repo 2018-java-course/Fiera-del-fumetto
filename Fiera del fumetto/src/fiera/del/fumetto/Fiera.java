8f8e995ff4859b525b1ccb1c070c5d0a93f0f6ef
PC20 <PC20@DESKTOP-D8D9GC6.fritz.box>
2018-07-18 15:55:08 +0200

    Project FieraFumetto Alpha 0.1
    Creazione classe Fiere, Creazione ArrayList di tipo fiera con successivo popolamento.

---
+++ Fiera del fumetto/src/fiera/del/fumetto/Fiera.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Fiera {
+
+    private String nomeEvento;
+    private String luogoEvento;
+    private int numPersone;
+
+    /*public Fiera inserimento(String nomeEvento, String luogoEvento){
+         Fiera myFiera = new Fiera(nomeEvento, luogoEvento);
+         return myFiera;
+    }*/
+
+    public Fiera(String nomeEvento, String luogoEvento)
+    {
+        this.nomeEvento = nomeEvento;
+        this.luogoEvento = luogoEvento;
+    }
+
+    /*public  ordinaFiera(){
+
+    }*/
+
+
+}
